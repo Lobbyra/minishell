b36f2be65728dbc14e58eff4957b9d3ad37dc413
lobbyra <jecaudal@student.42.fr>
2020-06-22 14:00:09 +0200

    :recycle: refactor : ft_memcpy_n.c parameters renamed

--- srcs/lib/ft_memcpy_n.c
+++ srcs/lib/ft_memcpy_n.c
@@ -6,24 +6,26 @@
 /*   By: jecaudal <jecaudal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/20 10:17:50 by jecaudal          #+#    #+#             */
-/*   Updated: 2020/06/13 16:11:04 by jecaudal         ###   ########.fr       */
+/*   Updated: 2020/06/22 13:59:05 by jecaudal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	*ft_memcpy_n(void *s1, void *s2, unsigned int n)
-{
-	unsigned char *t_s1;
-	unsigned char *t_s2;
+#include "minishell.h"
 
-	t_s1 = (unsigned char *)s1;
-	t_s2 = (unsigned char *)s2;
-	while (t_s1 - (unsigned char*)s1 < n)
+void	*ft_memcpy_n(void *dst, void *src, unsigned int n)
+{
+	unsigned char *t_dst;
+	unsigned char *t_src;
+
+	t_dst = (unsigned char *)dst;
+	t_src = (unsigned char *)src;
+	while (t_dst - (unsigned char*)dst < n)
 	{
-		*t_s1 = *t_s2;
-		t_s1++;
-		t_s2++;
+		*t_dst = *t_src;
+		t_dst++;
+		t_src++;
 	}
-	return (t_s1);
+	return (t_dst);
 }
