2cbb2d8eda684d1fef635066a6561b7a05c9fce1
lobbyra <jecaudal@student.42.fr>
2020-07-03 14:58:42 +0200

    :bug: fix : Fix init_pipes
    - When there was nothing in a command, init_pipes alloc n_jobs - 1 block
    so there was buffer_overflow and malloc incorrect value cause a crash.

--- srcs/execution/init_pipes.c
+++ srcs/execution/init_pipes.c
@@ -6,7 +6,7 @@
 /*   By: jecaudal <jecaudal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/29 15:41:42 by jecaudal          #+#    #+#             */
-/*   Updated: 2020/06/30 17:43:11 by jecaudal         ###   ########.fr       */
+/*   Updated: 2020/07/03 14:54:20 by jecaudal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,12 +31,16 @@ int			init_pipes(int **pipes, int n_jobs)
 	int n_pipes;
 
 	i = 0;
-	n_pipes = (n_jobs - 1) * 2;
+	if (n_jobs == 0)
+		n_pipes = 0;
+	else
+		n_pipes = (n_jobs - 1) * 2;
 	if (!(*pipes = (int*)malloc(sizeof(int) * (n_pipes))))
 		return (ERR_MALLOC);
 	while (i / 2 < n_jobs - 1)
 	{
-		pipe((*pipes) + i);
+		if ((pipe((*pipes) + i) == -1))
+			return (panic_init_pipes(*pipes));
 		i += 2;
 	}
 	return (0);
