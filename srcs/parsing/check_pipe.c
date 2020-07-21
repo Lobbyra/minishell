1fb4055ed9ef8125ec336ca10e9374e8a8abfd33
Jean-xavier RELIGIEUX <religieuxjxavier@gmail.com>
2020-07-21 16:21:51 +0200

    :arrow_up: feat : Modif Check_pipe | Check_quote
    - Check pipe and quote after reading cmd

--- srcs/parsing/check_pipe.c
+++ srcs/parsing/check_pipe.c
@@ -6,7 +6,7 @@
 /*   By: jereligi <jereligi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/20 13:51:24 by jereligi          #+#    #+#             */
-/*   Updated: 2020/07/21 14:45:16 by jereligi         ###   ########.fr       */
+/*   Updated: 2020/07/21 16:19:46 by jereligi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ static int		additional_read_pipe(t_stock *s)
 	l_printf("pipe> ");
 	status = get_next_line(0, &tmp);
 	if (status == GNL_LINE_READED || status == GNL_EOF)
-		s->user_input = ft_strjoindel(s->user_input, tmp, 3);
+		s->buf_user_input = ft_strjoindel(s->buf_user_input, tmp, 3);
 	else if (status == GNL_CTRLD)
 	{
 		s->error_strings = ft_strjoindel(s->error_strings, ERR_PIPE1, 1);
@@ -37,22 +37,22 @@ static int		additional_read_pipe(t_stock *s)
 		return (ERR_SYNTAX);
 	}
 	if (s->is_debug == TRUE)
-		l_printf("|%s|\n", s->user_input);
+		l_printf("|%s|\n", s->buf_user_input);
 	return (check_quote(s));
 }
 
 int				check_pipe_utils(t_stock *s, int *i)
 {
 	(*i)++;
-	while (ft_isprint(s->user_input[*i]) == 0 && s->user_input[*i])
+	while (ft_isprint(s->buf_user_input[*i]) == 0 && s->buf_user_input[*i])
 		(*i)++;
-	if (s->user_input[*i] == '\0' && s->is_cmd_closed == 1)
+	if (s->buf_user_input[*i] == '\0' && s->is_cmd_closed == 1)
 	{
 		s->error_strings = ft_strjoindel(s->error_strings, ERR_PIPE3, 1);
 		s->is_exec_abort = TRUE;
 		return (ERR_CRITIC);
 	}
-	else if (s->user_input[*i] == '\0')
+	else if (s->buf_user_input[*i] == '\0')
 	{
 		if (additional_read_pipe(s) == ERR_SYNTAX)
 			return (ERR_SYNTAX);
@@ -70,14 +70,15 @@ int				check_pipe(t_stock *s)
 	valid_pipe = 0;
 	while (valid_pipe == 0)
 	{
-		if (s->user_input[i] == '\0')
+		if (s->buf_user_input[i] == '\0')
 			valid_pipe = 1;
-		if (i == 0 && s->user_input[i] == '|')
+		if (i == 0 && s->buf_user_input[i] == '|')
 		{
 			s->error_strings = ft_strjoindel(s->error_strings, ERR_PIPE2, 1);
 			return (ERR_SYNTAX);
 		}
-		else if (s->user_input[i] == '|' && s->user_input[i - 1] != '\\')
+		else if (s->buf_user_input[i] == '|' && 
+		s->buf_user_input[i - 1] != '\\')
 			return (check_pipe_utils(s, &i));
 		i++;
 	}
