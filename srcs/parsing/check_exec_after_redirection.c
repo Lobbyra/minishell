ef103c28ed117ca8cbbc367e6919ce665bd607ec
Jean-xavier RELIGIEUX <religieuxjxavier@gmail.com>
2020-07-22 17:06:45 +0200

    :bug: fix : error condition (segfault)

--- srcs/parsing/check_exec_after_redirection.c
+++ srcs/parsing/check_exec_after_redirection.c
@@ -6,7 +6,7 @@
 /*   By: jereligi <jereligi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/21 14:34:56 by jereligi          #+#    #+#             */
-/*   Updated: 2020/07/21 14:37:48 by jereligi         ###   ########.fr       */
+/*   Updated: 2020/07/22 17:06:00 by jereligi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,14 @@ int		check_exec_after_redirection(t_stock *s, int n, int *status, char *exec)
 	int	i;
 
 	i = 0;
-	if (exec)
+	while (s->jobs[n][i])
+		i++;
+	if (i > 3)
 	{
-		while (s->jobs[n][i])
-			i++;
-		if (i > 3)
-		{
-			exec = ft_strjoin("/", s->jobs[n][2]);
-			*status = 1;
-		}
-		else
-			return (0);
+		exec = ft_strjoin("/", s->jobs[n][2]);
+		*status = 1;
 	}
+	else
+		return (0);
 	return (1);
 }
